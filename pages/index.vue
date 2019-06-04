ef0d90f5b837ecaf78f564ec36cabdea57007249
Collin Messer <collin@collinmesser.com>
2019-06-04 08:03:24 -0400

    Add Hubspot code

--- pages/index.vue
+++ pages/index.vue
@@ -34,6 +34,9 @@
         </div>
       </div>
     </section>
+    <!-- Start of HubSpot Embed Code -->
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/5969604.js"></script>
+    <!-- End of HubSpot Embed Code -->
   </div>
 </template>
 
